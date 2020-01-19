94ff2ea14f043dbf70189967cd1b3ba577e7a97f
meti <metibizu@gmail.com>
2020-01-19 13:38:11 -0800

    first commit

---
+++ CRUDWeb/CRUDWeb/Controllers/EmpCrudController.cs
@@ -0,0 +1,103 @@
+﻿using CrudDbContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDWeb.Controllers
+{
+    public class EmpCrudController : Controller
+    {
+        CrudDbDataContext context = new CrudDbDataContext();
+        // GET: EmpCrud
+        public ActionResult Index()
+        {
+            var empdetails = from x in context.EmpTables select x;
+            return View(empdetails);
+        }
+
+        // GET: EmpCrud/Details/5
+        public ActionResult Details(int id)
+        {
+            var getempdetails = context.EmpTables.Single(x => x.EmpId == id);
+            return View(getempdetails);
+        }
+
+        // GET: EmpCrud/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmpCrud/Create
+        [HttpPost]
+        public ActionResult Create(EmpTable collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                context.EmpTables.InsertOnSubmit(collection);
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpCrud/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var getempdetails = context.EmpTables.Single(x => x.EmpId == id);
+            return View(getempdetails);
+        }
+
+        // POST: EmpCrud/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, EmpTable collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                EmpTable empupdate = context.EmpTables.Single(x => x.EmpId == id);
+                empupdate.EmpName = collection.EmpName;
+                empupdate.Email = collection.Email;
+                empupdate.Salary = collection.Salary;
+                context.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpCrud/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var getempdetails = context.EmpTables.Single(x => x.EmpId == id);
+            return View(getempdetails);
+        }
+
+        // POST: EmpCrud/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, EmpTable collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var empdel = context.EmpTables.Single(x => x.EmpId == id);
+                context.EmpTables.DeleteOnSubmit(collection);
+                context.SubmitChanges();
+                
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ xml_persistant_model/xml_persistant_model/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace xml_persistant_model
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Person> p1 = new List<Person>();
+            XmlSerializer serial = new XmlSerializer(typeof(List<Person>));
+            p1.Add(new Person() { Id = 1, Name = "Abebe" }); 
+            p1.Add(new Person() { Id = 2, Name = "Kebede"});
+            using (FileStream fs = new FileStream(Environment.CurrentDirectory + "\\xmlfile.xml", FileMode.Create, FileAccess.Write))
+            {
+                serial.Serialize(fs, p1);
+                MessageBox.Show("File Created");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Person> p1 = new List<Person>();
+            XmlSerializer serial = new XmlSerializer(typeof(List<Person>));
+       
+            using (FileStream fs = new FileStream(Environment.CurrentDirectory + "\\xmlfile.xml", FileMode.Open, FileAccess.Read))
+            {
+                p1 = serial.Deserialize(fs) as List<Person>;
+            }
+            dataGridView1.DataSource = p1;
+        }
+    }
+}
